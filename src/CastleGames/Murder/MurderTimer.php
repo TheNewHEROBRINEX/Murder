f9432c1fa7464a2e692e9f780e572e35f3167abb
TheNewHEROBRINEX <luca.biazzetti@gmail.com>
2017-01-05 18:30:24 +0100

    Update MurderTImer.php

--- src/CastleGames/Murder/MurderTimer.php
+++ src/CastleGames/Murder/MurderTimer.php
@@ -2,16 +2,16 @@
 
 namespace CastleGames\Murder;
 
-
 use pocketmine\scheduler\PluginTask;
-
 
 class MurderTimer extends PluginTask {
 
-    public function onRun($tick) {
-    
-        foreach ($this->getOwner()->getArenas() as $Murdername => $Murderarena)
-            $Murderarena->tick();
+    public function __construct(Main $owner) {
+        parent::__construct($owner);
+    }
 
+    public function onRun($tick) {
+        foreach ($this->getOwner()->getArenas() as $arena)
+            $arena->tick();
     }
 }
