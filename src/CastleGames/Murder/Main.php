0e646fe9f4fd35364b103a78579174a66c6b6ad5
TheNewHEROBRINEX <luca.biazzetti@gmail.com>
2017-01-05 15:30:52 +0100

    Add joining

--- src/CastleGames/Murder/Main.php
+++ src/CastleGames/Murder/Main.php
@@ -10,37 +10,59 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
-	private $config, $listener;
+    /** @var Config $config */
+    private $config;
 
-	public function onEnable() {
-		@mkdir($this->getDataFolder());
-		$this->getServer()->getPluginManager()->registerEvents($this->listener = new MurderListener($this), $this);
-		$this->config = new Config($this->getDataFolder() . "config.yml");
-	}
+    /** @var MurderListener $listener */
+    private $listener;
 
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args) : bool {
-		if ($sender instanceof Player) {
-			if (isset($args[0])) {
-				switch (array_shift($args)) {
-					case "join":
-						//TODO
-						break;
-					case "quit":
-						//TODO
-						break;
-					case "setspawns":
-						$world = $sender->getLevel()->getName();
-						$name = $sender->getName();
-						if ($sender->hasPermission("murder.command.setspawns"))
-							if (isset($args[0]) && is_numeric($args[0])) {
-								$this->listener->setspawns[$name][$world] = (int)$args[0];
-								$this->getConfig()->remove($world);
-								$sender->sendMessage("§eSettaggio di§f $args[0] §espawn per il mondo§f {$sender->getLevel()->getName()} §einiziato");
-							}
-						break;
-				}
-			}
-		}
-		return true;
-	}
+    /** @var  MurderArena[] $arenas */
+    private $arenas;
+
+    public function onEnable() {
+        @mkdir($this->getDataFolder());
+        $this->getServer()->getPluginManager()->registerEvents($this->listener = new MurderListener($this), $this);
+        $this->config = new Config($this->getDataFolder() . "config.yml");
+        foreach ($this->getConfig()->getAll() as $name => $spawns) {
+            $this->addArena($name, $spawns);
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param array $spawns
+     * @param int $countdown
+     * @param int $maxTime
+     * @return MurderArena
+     */
+    public function addArena(string $name, array $spawns, $countdown = 60, $maxTime = 1200) {
+        return $this->arenas[$name] = new MurderArena($this, $spawns, $name, $countdown, $maxTime);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) : bool {
+        if ($sender instanceof Player) {
+            if (isset($args[0])) {
+                switch (array_shift($args)) {
+                    case "join":
+                        if (isset($this->arenas[$args[0]]))
+                            $this->arenas[$args[0]]->join($sender);
+                        break;
+                    case "quit":
+                        //TODO
+                        break;
+                    case "setspawns":
+                        $world = $sender->getLevel()->getName();
+                        $name = $sender->getName();
+                        if ($sender->hasPermission("murder.command.setspawns"))
+                            if (isset($args[0]) && is_numeric($args[0])) {
+                                $this->listener->setspawns[$name][$world] = (int)$args[0];
+                                $this->getConfig()->remove($world);
+                                $sender->sendMessage("§eSettaggio di§f $args[0] §espawn per il mondo§f {$sender->getLevel()->getName()} §einiziato");
+                            }
+                        break;
+                }
+            }
+        }
+        return true;
+    }
 }
---
+++ src/CastleGames/Murder/MurderArena.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace CastleGames\Murder;
+
+use pocketmine\level\Position;
+use pocketmine\Player;
+
+class MurderArena {
+
+    const GAME_IDLE = 0;
+    const GAME_STARTING = 1;
+    const GAME_RUNNING = 2;
+
+    /** @var Main $plugin */
+    private $plugin;
+
+    /** @var array $spawns */
+    private $spawns;
+
+    /** @var string $name */
+    private $name;
+
+    /** @var int $countdown */
+    private $countdown;
+
+    /** @var int $maxTime */
+    private $maxTime;
+
+    /** @var int $status */
+    private $status = self::GAME_IDLE;
+
+    /** @var Player[] $players */
+    private $players = [];
+
+    /**
+     * Arena constructor.
+     * @param Main $plugin
+     * @param array $spawns
+     * @param string $name
+     * @param int $countdown
+     * @param int $maxtime
+     */
+    public function __construct(Main $plugin, array $spawns, string $name, int $countdown, int $maxTime) {
+        $this->plugin = $plugin;
+        $this->spawns = $spawns;
+        $this->name = $name;
+        $this->countdown = $countdown;
+        $this->maxTime = $maxTime;
+    }
+
+    public function join(Player $player){
+        $spawn = array_shift($this->spawns);
+        $this->players[$player->getName()] = $spawn;
+        $player->teleport(new Position(...$spawn, $this->plugin->getServer()->getLevelByName($this->name)));
+    }
+
+    /**
+     * @return bool
+     */
+    public function isIdle() {
+        return $this->status == 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStarting() {
+        return $this->status == 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRunning() {
+        return $this->status == 2;
+    }
+}
