dcd88d3dba17f7eda77a47ec07d279ef5cb3f962
Kenner-G <kenner.galdamez@gmail.com>
2017-08-19 13:42:14 -0600

    Create MurderTimer.php

---
+++ English/src/TheNewHEROBRINEX/Murder/MurderTimer.php
@@ -0,0 +1,15 @@
+<?php
+namespace TheNewHEROBRINE\Murder;
+use pocketmine\scheduler\PluginTask;
+class MurderTimer extends PluginTask {
+    /**
+     * @param int $tick
+     */
+    public function onRun(int $tick) {
+        if ($this->owner instanceof MurderMain){
+            foreach ($this->owner->getArenas() as $arena) {
+                $arena->tick();
+            }
+        }
+    }
+}
