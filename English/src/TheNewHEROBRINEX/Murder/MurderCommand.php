75c1e62a21d2436ed62b451bff6d54b64275b302
Kenner-G <kenner.galdamez@gmail.com>
2017-08-19 13:59:09 -0600

    Translated to English

--- English/src/TheNewHEROBRINEX/Murder/MurderCommand.php
+++ English/src/TheNewHEROBRINEX/Murder/MurderCommand.php
@@ -15,7 +15,7 @@ class MurderCommand extends Command implements PluginIdentifiableCommand {
      * @param MurderMain $plugin
      */
     public function __construct(MurderMain $plugin) {
-        parent::__construct("murder", "Comando principale del minigame Murder", "/murder join {arena}|quit|setarena {players} {emeralds}", ["mdr"]);
+        parent::__construct("murder", "The main Murder plugin command", "/murder join {arena}|quit|setarena {players} {emeralds}", ["mdr"]);
         $this->setPermission("murder.command.join;murder.command.quit;murder.command.setarena");
         $this->plugin = $plugin;
     }
@@ -30,7 +30,7 @@ class MurderCommand extends Command implements PluginIdentifiableCommand {
             return true;
         }
         if (!$sender instanceof Player){
-            $sender->sendMessage(TextFormat::RED . "Puoi eseguire questo comando solo in-game!");
+            $sender->sendMessage(TextFormat::RED . "You can only run this command in-game!");
             return true;
         }
         if (count($args) === 0 or count($args) > 3){
@@ -51,7 +51,7 @@ class MurderCommand extends Command implements PluginIdentifiableCommand {
                     $arena->join($sender);
                 }
                 else{
-                    $sender->sendMessage(TextFormat::RED . "L'arena $args[0] non esiste!");
+                    $sender->sendMessage(TextFormat::RED . "The arena named $args[0] does not exist!");
                 }
                 return true;
             case "quit":
@@ -65,7 +65,7 @@ class MurderCommand extends Command implements PluginIdentifiableCommand {
                     $arena->quit($sender);
                 }
                 else{
-                    $sender->sendMessage(TextFormat::RED . "Non sei in nessuna partita di Murder!");
+                    $sender->sendMessage(TextFormat::RED . "You are not in any Murder game!");
                 }
                 return true;
             case "setarena":
@@ -81,7 +81,7 @@ class MurderCommand extends Command implements PluginIdentifiableCommand {
                 $this->getPlugin()->getListener()->setespawns[$name][$world] = (int)$args[1];
                 $this->getPlugin()->getArenasCfg()->setNested("$world.spawns", []);
                 $this->getPlugin()->getArenasCfg()->setNested("$world.espawns", []);
-                $this->getPlugin()->sendMessage("§eSettaggio di§f $args[0] §espawn per il mondo§f {$sender->getLevel()->getFolderName()} §einiziato", $sender);
+                $this->getPlugin()->sendMessage("§eSetting §f $args[0] §eas the spawn for the world§f {$sender->getLevel()->getFolderName()} §eReady to go!", $sender);
                 return true;
                 
             default:
