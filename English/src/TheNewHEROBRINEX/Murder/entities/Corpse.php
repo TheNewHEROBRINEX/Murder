d5b1ad775d9c4b82db65bec3f13b4ff156cb8cb1
Kenner-G <kenner.galdamez@gmail.com>
2017-08-19 13:44:43 -0600

    Create Corpse.php

---
+++ English/src/TheNewHEROBRINEX/Murder/entities/Corpse.php
@@ -0,0 +1,46 @@
+<?php
+namespace TheNewHEROBRINE\Murder\entities;
+use pocketmine\entity\Human;
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\Player;
+class Corpse extends Human {
+    /**
+     * @param Level $level
+     * @param CompoundTag $nbt
+     * @param Player $player
+     */
+    public function __construct(Level $level, CompoundTag $nbt, Player $player = null) {
+        if ($player === null){
+            $this->close();
+        }
+        else{
+            $nbt = new CompoundTag("", [
+                "Pos" => new ListTag("Pos", [
+                    new DoubleTag("", $player->x),
+                    new DoubleTag("", $player->y),
+                    new DoubleTag("", $player->z)
+                ]),
+                "Motion" => new ListTag("Motion", [
+                    new DoubleTag("", 0),
+                    new DoubleTag("", 0),
+                    new DoubleTag("", 0)
+                ]),
+                "Rotation" => new ListTag("Rotation", [
+                    new FloatTag("", $player->yaw),
+                    new FloatTag("", $player->pitch)
+                ]),
+            ]);
+            $player->saveNBT();
+            $nbt->Inventory = clone $player->namedtag->Inventory;
+            $nbt->Skin = new CompoundTag("Skin", ["Data" => new StringTag("Data", $player->getSkinData()), "Name" => new StringTag("Name", $player->getSkinId())]);
+            parent::__construct($level, $nbt);
+            $this->setDataProperty(Human::DATA_PLAYER_BED_POSITION, Human::DATA_TYPE_POS, [(int)$player->x, (int)$player->y, (int)$player->z]);
+            $this->setDataFlag(Human::DATA_PLAYER_FLAGS, Human::DATA_PLAYER_FLAG_SLEEP, true, Human::DATA_TYPE_BYTE);
+        }
+    }
+}
